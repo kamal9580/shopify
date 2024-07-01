fd31359d8680511c36fe828986db3fe98c18b942
kamal9580 <nileshprince3118@gmail.com>
2024-07-01 13:28:28 +0530

    message

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/src/components/Hero";
+import NewProducts from "@/src/components/NewProducts";
+import Testimonial from "@/src/components/Testimonial";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <NewProducts />
+      <Testimonial />
+    </main>
+  );
+}
